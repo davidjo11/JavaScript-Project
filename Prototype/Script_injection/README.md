b9f5e28c15ccbefceea3c47cc4aec2f1ac46e3dc
flament <flament.mickael@gmail.com>
2015-09-24 11:13:54 +0200

    Première version de la récupération de recette, sans sauvegarde

--- Prototype/Script_injection/README.md
+++ Prototype/Script_injection/README.md
@@ -1,2 +1,8 @@
-# JavaScript-Project
-The JavaScript project.
+# Récupération des recettes
+
+## Comment faire ?
+
+1. Accéder à la page index.html du dossier
+2. Glisser le bouton dans votre barre de favoris
+3. Aller sur le site de marmitton, choisissez une recette
+4. Appuyer sur le bouton dans les favoris pour récupérer la recette
--- Prototype/script_injection/injection_code.js
+++ Prototype/script_injection/injection_code.js
@@ -1,102 +1,48 @@
-//
-//javascript:{var baliseScript = document.createElement('script');baliseScript.src = "http://localhost:63342/TP2/script.js";document.getElementsByTagName('body')[0].appendChild(baliseScript);}
-var oldButton = document.getElementById('__fond');
-if(oldButton)
-    oldButton.parentNode.removeChild(oldButton);
+function Parser(){}
 
-
-// FUNCTIONS
-function ce(type){
-    var element = document.createElement(type);
-
-    for(var i = 1, il = arguments.length; i < il ; i+=2){
-        element.style[arguments[i]] = arguments[i+1];
-    }
-
-    return element;
-}
-
-
-
-// OBJECT
-function Drawer(title){
-    this.textTitle = title;
-}
-
-Drawer.prototype = {
-    creationFond : function(){
-        var fond = document.createElement("div");
-        fond.style.backgroundColor = "rgba(0,0,0,0.5)";
-        fond.style.position = "fixed";
-        fond.style.top = "0";
-        fond.style.left = "0";
-        fond.style.width = "100%";
-        fond.style.height = "100%";
-        fond.style.zIndex = "300";
-        fond.id = "__fond";
-
-        fond.addEventListener('click', function() {
-            document.body.removeChild(fond);
-        });
-        this.fond = fond;
-    },
-    creationFondBlanc : function(){
-        var white = document.createElement("div");
-        white.style.position = "relative";
-        white.style.marginTop = "100px";
-        white.style.marginLeft = "auto";
-        white.style.marginRight = "auto";
-        white.style.width = "500px";
-        white.style.height = "300px";
-        white.style.backgroundColor = "white";
-        white.style.background = "#fff";
-        white.style.borderRadius = "4px";
-        white.style.boxShadow = "#000 2px 2px 5px";
-
-        white.onclick = function(event){event.stopPropagation()};
-        this.white = white;
-    },
-    creationTitle : function(){
-        var title = document.createElement('h1');
-        title.innerText = this.textTitle;
-        title.style.fontSize = "24px";
-        title.style.color = "#444";
-        title.style.borderBottom = "1px solid gray";
-        title.style.padding = "30px";
-        this.title = title;
-    },
-    creationButton : function(){
-        var button = ce(
-            "button",
-            "position", "absolute",
-            "bottom", "20px",
-            "right", "20px",
-            "width", "100px",
-            "height", "50px",
-            "borderRadius", "500px",
-            "backgroundColor", "lightGray",
-            "border", "1px solid gray"
-        );
-        button.innerText = "Ok";
-        this.button = button;
+Parser.prototype = {
+    getRecipeContent : function(){
+        var recipeElement = document.getElementsByClassName('m_content_recette_ingredients m_avec_substitution')[0];
+        return recipeElement.innerHTML;
     },
 
-    show : function(){
-        this.creationFond();
-        this.creationFondBlanc();
-        this.creationTitle();
-        this.creationButton();
+    /**
+     * this function extract ingredient from an html string
+     * @param text => expect dom elements as string which contains ingredients
+     * @returns {XML|string|*}
+     */
+    getIngredients : function(text){
+        // remove open link tag and its attributes
+        text = text.replace(/(<a[^<]*>)/g, '');
+        // remove close link tag
+        text = text.replace(/(<\/a>)/g, '');
+        // remove open link tag and its attributes
+        text = text.replace(/<span>(.*)<\/span>/g, '');
 
-        this.white.appendChild(this.title);
-        this.white.appendChild(this.button);
-        this.fond.appendChild(this.white);
-        document.getElementsByTagName('body')[0].appendChild(this.fond);
+        var text_table = text.split('<br>');
+
+
+        var ingredient_table = [];
+
+        // add each ingredient inside a table, it exclude title and delete white spaces
+        for(var i = 0, il = text_table.length ; i < il ; i++){
+            if(text_table[i].indexOf('-') !== -1){
+                ingredient_table.push(text_table[i].replace('-', '').trim());
+            }
+        }
+
+        return ingredient_table;
+    },
+
+    parse : function(){
+        var recipeElement = this.getRecipeContent();
+        var links = this.getIngredients(recipeElement);
+        console.log(links);
     }
 };
 
-var a = new Drawer("mon titre 2");
-a.show();
-
+var parser = new Parser();
+var recipeElements = parser.parse();
 
 /***
 
@@ -197,7 +143,7 @@ a.show();
 
  ===================
 
- CrÃ©er une classe Dialog qui hÃ©rite de Popup et qui rajoute un champ de saisie textuelle
+ Créer une classe Dialog qui hérite de Popup et qui rajoute un champ de saisie textuelle
 
  function Dialog (title, callBackFunction) {
    PopUp.call(this,title,callBackFunction);
