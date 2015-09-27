449874c0d94c218ba9f05086b42a46f3f4c18b1f
DavidTheProcrastinator <dav.josias@gmail.com>
2015-09-27 15:39:44 +0200

    Edited UserManagerClass content

--- media/script/UserManager.js
+++ media/script/UserManager.js
@@ -1,25 +1,25 @@
-function UserManager(){
-  this.usersList = new Array();
+function UserManager() {
+    this.doc = "Association socketId (key) - name (value) for all clients in the room.\n";
+    this.keyName = {};
+    this.nameKey = {};
 }
 
 UserManager.prototype = {
-  userExists : function(user){
-    return this.usersList.indexOf(user) > -1;
-  },
 
-  addUser : function (user){
-    if(!this.userExists(user) || user !== ""){
-      this.usersList.push(user);
-      return true;
-    }
-    else return false;
-  },
+    addUser: function (socketId, userName) {
+        if (this.key.name.socketId !== undefined || this.nameKey.name !== undefined) {
+            return false
+        } else {
+            this.keyName.socketId = userName;
+            this.nameKey.name = socketId;
+            return true;
+        }
 
-  removeUser : function (user){
-    if(userExists(user)){
-      this.usersList.splice(indexOf(user),1);
-      return true;
+    },
+
+    removeUser: function (user) {
+        var key = this.nameKey.user;
+        delete this.keyName.(this.nameKey.user);
+        return this.keyName.key === undefined;
     }
-    return false;
-  }
 }
