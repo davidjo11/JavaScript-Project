48da9bee4a4810e8b52e0e9097475d940f969b82
DavidTheProcrastinator <dav.josias@gmail.com>
2015-09-27 17:07:15 +0200

    2 kind of messages for now: one to create a list and another to warn other that I just join the room.

--- media/script/MessageCreator.js
+++ media/script/MessageCreator.js
@@ -1,4 +1,4 @@
-function MessageCreator(user, titre, description, produits){
+function MessageManager(user, titre, description, produits){
     this.doc = "La classe MessageCreator permet de créer les messages de créer ou de modifications de liste au format convenant avant de l'envoyer avec la méthode sendMessage de Cobra.\n"
         + "La partie \"content\", de ce message, contiendra un objet contenant ayant pour clés:\n"
         + "- la clé key_user: le nom de la personne qui a posté la liste, il ne contiendra que des caractères alphanumériques\n"
@@ -8,16 +8,12 @@ function MessageCreator(user, titre, description, produits){
         + "Chaque valeur associée aux différentes clés sera précédée d'un nombre permettant d'obtenir le nombre de carctères qui composent la valeur, si elle était une chaine de caractères, afin de faciliter son extraction.\n" ;
 }
 
-MessageCreator.prototype = {
+MessageManager.prototype = {
+    createMessageJoin :
+
   createMessage : function (user, title, description, products){
-    user = ((user).length + 1) + user;
-      title = ((title).length + 1) + title;
-      description = ((description.length) + 1) + description;
-      return {
-        key_user : user,
-        key_titre : title,
-        key_description: description,
-        key_products : products
+    return {
+        user
     }
   }
 };
