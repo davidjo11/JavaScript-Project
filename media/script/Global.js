03842aeb9d30a0d0eb383aa108d530fc3a201976
DavidTheProcrastinator <dav.josias@gmail.com>
2015-09-27 15:13:16 +0200

    Add getRandomColors

--- media/script/Global.js
+++ media/script/Global.js
@@ -1,79 +1,103 @@
 //Lancer les traitements après le chargement de la page de sorte à pouvoir
 // récupérer le body (faire onload() sur body).
 Tools = {
-  include : function(url, callback){
+    include: function (url, callback) {
 
-    /* on crée une balise<script type="text/javascript"></script> */
-    var script = document.createElement('script');
-    script.type = 'text/javascript';
+        /* on crée une balise<script type="text/javascript"></script> */
+        var script = document.createElement('script');
+        script.type = 'text/javascript';
 
-    /* On fait pointer la balise sur le script qu'on veut charger
-    avec en prime un timestamp pour éviter les problèmes de cache
-    */
+        /* On fait pointer la balise sur le script qu'on veut charger
+        avec en prime un timestamp pour éviter les problèmes de cache
+        */
 
-    script.src = url + '?' + (new Date().getTime());
+        script.src = url + '?' + (new Date().getTime());
 
-    /* On dit d'exécuter cette fonction une fois que le script est chargé */
-    if (callback) {
-      script.onreadystatechange = callback;
-      script.onload = script.onreadystatechange;
+        /* On dit d'exécuter cette fonction une fois que le script est chargé */
+        if (callback) {
+            script.onreadystatechange = callback;
+            script.onload = script.onreadystatechange;
+        }
+
+        /* On rajoute la balise script dans le head, ce qui démarre le téléchargement */
+        document.getElementsByTagName('head')[0].appendChild(script);
+    },
+
+    createStyledElement: function (tagName) {
+        var element = document.createElement(tagName);
+        for (var i = 1; i < arguments.length; i += 2) {
+            element.style[arguments[i]] = arguments[i + 1];
+        }
+        return element;
+    },
+
+    assignAttributes: function (balise) {
+        for (i = 1; i < arguments.length; i++) {
+            balise.setAttribute(arguments[i++], arguments[i]);
+        }
+    },
+
+    // renvoie la balise body du document
+    getBody: function () {
+        return document.getElementsByTagName("body")[0];
+    },
+
+    // ajouter une balise nomBalise � la balise baliseMere
+    ajouterBalise: function (baliseMere, nomBalise) {
+        baliseMere.appendChild(nomBalise);
+    },
+
+    // il est possible de passer des attributs et leur valeur en param�tres � l'infini
+    // ex : ajouterBalise (document.getElementById("i0"),"P","align","center","valign","middle");
+    creerBalise: function (nomBalise) {
+        var newBalise = document.createElement(nomBalise);
+        for (i = 1; i < arguments.length; i++)
+            newBalise.setAttribute(arguments[i++], arguments[i]);
+        return newBalise;
+    },
+
+    // ajoute (et renvoie) une balise textuelle � balise
+    // ex : ajouterBalise (document.getElementById("i0"),"bonjour");
+    ajouterTexte: function (balise, texte) {
+        var text = document.createTextNode(texte);
+        balise.appendChild(text);
+    },
+
+    getRandomColors: function (nbColors) {
+        var colors = "#FFFFFF";
+        var letters = '0123456789ABCDEF'.split('');
+        for (var i = 0; i < nbColors; i) {
+            color = '#';
+            for (var j = 0; j < 6; j++) {
+                color += letters[Math.floor(Math.random() * 16)];
+            }
+            if(colors.indexOf(color) === -1){
+                colors += ";" + color;
+                i++;
+            }
+        }
+        return colors.split(";");
     }
-
-    /* On rajoute la balise script dans le head, ce qui démarre le téléchargement */
-    document.getElementsByTagName('head')[0].appendChild(script);
-  },
-
-  createStyledElement: function (tagName) {
-    var element = document.createElement(tagName);
-    for (var i = 1; i < arguments.length; i += 2) {
-      element.style[arguments[i]] = arguments[i + 1];
-    }
-    return element;
-  },
-
-  assignAttributes : function (balise){
-    for(i = 1;i<arguments.length;i++){
-      balise.setAttribute(arguments[i++], arguments[i]);
-    }
-  },
-
-  // renvoie la balise body du document
-  getBody : function(){
-    return document.getElementsByTagName("body")[0];
-  },
-
-  // ajouter une balise nomBalise � la balise baliseMere
-  ajouterBalise : function(baliseMere, nomBalise){
-    baliseMere.appendChild(nomBalise);
-  },
-
-  // il est possible de passer des attributs et leur valeur en param�tres � l'infini
-  // ex : ajouterBalise (document.getElementById("i0"),"P","align","center","valign","middle");
-  creerBalise : function(nomBalise){
-    var newBalise = document.createElement(nomBalise);
-    for(i=1;i<arguments.length;i++)
-    newBalise.setAttribute(arguments[i++], arguments[i]);
-    return newBalise;
-  },
-
-  // ajoute (et renvoie) une balise textuelle � balise
-  // ex : ajouterBalise (document.getElementById("i0"),"bonjour");
-  ajouterTexte : function(balise, texte){
-    var text = document.createTextNode(texte);
-    balise.appendChild(text);
-  }
 }
 
 page = undefined;
 users = undefined;
-msgCreator =  undefined;
+msgCreator = undefined;
+user = undefined;
 
-Tools.include('media/script/Initializator.js', function(){ page = new PageBundle();});
-Tools.include('media/script/UserManager.js', function(){ userMan = new UserManager();});
-Tools.include('media/script/MessageCreator.js', function (){ msgCreator = new MessageCreator();});
-Tools.include('media/script/User.js', function (){ });
+
+Tools.include('media/script/PageManager.js', function () {
+    page = new PageManager();
+});
+Tools.include('media/script/UserManager.js', function () {
+    userMan = new UserManager();
+});
+Tools.include('media/script/MessageCreator.js', function () {
+    msgCreator = new MessageCreator();
+});
+Tools.include('media/script/User.js', function () {});
+
 /*
-
 document.addEventListener("click", function (){
   var target = event.getTarget();
   if(target.id == "ValiderConnexion"){
