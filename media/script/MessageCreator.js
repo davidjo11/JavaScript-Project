8c4e58c2e8bcb2a121100bd6d9d389172cbb250f
DavidTheProcrastinator <dav.josias@gmail.com>
2015-09-27 12:24:58 +0200

    Edites MessageCreator & its method.

--- media/script/MessageCreator.js
+++ media/script/MessageCreator.js
@@ -1,17 +1,15 @@
 function Message(user, titre, description, produits){
-  this.user = user;
-  this.content = {
-  titre : titre,
-  description : description,
-  produits : produits
-  };
+    this.doc = "La classe MessageCreator permet de créer les messages de créer ou de modifications de liste au format convenant."
+        + "" ;
 }
 
 Message.prototype = {
-  setMessage : function (user, titre, description, produits){
-    this.user = user;
-    this.content.titre = titre;
-    this.content.description = description;
-    this.content.produits = produits;
+  createMessage : function (user, title, description, products){
+    return {
+        user : user,
+        titre : title,
+        description: description,
+        products : products
+    }
   }
 };
