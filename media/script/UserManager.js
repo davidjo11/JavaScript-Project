02bbbc93ff00727c6890a42fa876a9f8339e5b4b
DavidTheProcrastinator <dav.josias@gmail.com>
2015-09-27 18:06:02 +0200

    Conflicts resolved

--- media/script/UserManager.js
+++ media/script/UserManager.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 function UserManager() {
     this.doc = "Association socketId (key) - name (value) for all clients in the room.\n";
     this.keyName = {};
@@ -23,3 +24,30 @@ UserManager.prototype = {
         return this.keyName.key === undefined;
     }
 }
+=======
+function UserManager(){
+  this.usersList = new Array();
+}
+
+UserManager.prototype = {
+  userExists : function(user){
+    return this.usersList.indexOf(user) > -1;
+  },
+
+  addUser : function (user){
+    if(!this.userExists(user) || user !== ""){
+      this.usersList.push(user);
+      return true;
+    }
+    else return false;
+  },
+
+  removeUser : function (user){
+    if(userExists(user)){
+      this.usersList.splice(indexOf(user),1);
+      return true;
+    }
+    return false;
+  }
+}
+>>>>>>> 517402cd954da68d0b20c490a7d8a9a3bf99fd14
