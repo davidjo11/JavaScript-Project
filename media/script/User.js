ac89a7e7beb6dce11fd87a49880d087b9cf247f4
DavidTheProcrastinator <dav.josias@gmail.com>
2015-09-30 17:38:41 +0200

    Add this.list, the rest was because of some reason... I don't know...

--- media/script/User.js
+++ media/script/User.js
@@ -1,32 +1,33 @@
-function User(socket){
-  this.socketId = socket ;
-    this.name = undefined;
-//  this.password = password;
+function User(username, socket) {
+    this.socketId = socket;
+    this.name = username;
+    //  this.password = password;
     this.color = undefined;
+    this.lists = [];
 }
 
 User.prototype = {
-  getName : function (){
-    return this.name;
-  },
-
-//  getPassword : function (){
-//    return this.password;
-//  },
-
-    getSocket: function (){
-        return this.socketId:
+    getName: function () {
+        return this.name;
     },
 
-    setColor : function (color){
+    //  getPassword : function (){
+    //    return this.password;
+    //  },
+
+    getSocket: function () {
+        return this.socketId;
+    },
+
+    setColor: function (color) {
         this.color = color;
     },
 
-    getColor: function (){
+    getColor: function () {
         return this.color;
     },
 
-  equals : function (user2){
-    return typeOf(user2) === "User" && user2.getName() === this.getName() && this.getSocket() === user2.getSocket();
-  }
+    equals: function (user2) {
+        return typeOf(user2) === "User" && user2.getName() === this.getName() && this.getSocket() === user2.getSocket();
+    }
 };
