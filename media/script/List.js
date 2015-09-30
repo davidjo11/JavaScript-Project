858d2d2ad0c2cb105d0944b0b7d40248b3b2ff34
DavidTheProcrastinator <dav.josias@gmail.com>
2015-09-30 22:26:53 +0200

    New function toHtml().

--- media/script/List.js
+++ media/script/List.js
@@ -1,4 +1,5 @@
 function List(title, prop) {
+    this.isBeingEdited = false;
     this.name = title;
     this.proprietor = prop;
     this.products = [];
@@ -38,18 +39,18 @@ List.prototype = {
     },
 
     addUser: function (user) {
-        var aux = this.sharedWith.indexOf((user);
-        if (aux === -1) {
+        var aux = this.getUser(user);
+        if (aux === undefined) {
             this.sharedWith.push(user);
             return true;
         }
         return false;
     },
 
-    indexOf: function (user) {
+    getUser: function (user) {
         for (var i = 0; i < this.sharedWith.length; i++) {
             var u = this.sharedWith.[i];
-            if (u.equals(user)) {
+            if (u.equals(user.getSocket())) {
                 return i;
             }
         }
@@ -67,7 +68,34 @@ List.prototype = {
 
     setDescription: function (desc) {
         this.description = desc;
+    },
+
+    toHtml: function () {
+        'use strict';
+        var l = Tools.createStyledElement("fieldset",
+                                        "border-top-color", this.prop.getColor(),
+                                        );
+        Tools.assignAttributes(l,
+                                "id", "list_"+this.name,
+                                "class","list");
+
+        var legend = Tools.createStyledElement("legend",
+                                         "background-color", ""+this.prop.getColor());
+        Tools.ajouterBalise(l, legend);
+
+        var desc = Tools.createStyledElement("div");
+        Toolsµ.ajouterBalise(desc, this.description);
+        Tools.ajouterTexte(desc, this.description);
+        Tools.ajouterBalise(l, desc);
+
+        var products = Tools.createStyledElement("div");
+        for(var i=0;i<this.products.length;i++){
+            var p = Tools.createStyledElement("span");
+            Tools.ajouterTexte(p, ""+this.products[i]);
+            Tools.ajouterBalise(products, p);
+        }
+        Tools.ajouterBalise(l, products);
+
+        return l;
     }
-
-
 }
